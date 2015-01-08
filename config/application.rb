94918fcfb9a5894f47225774d4b800726f3e0b4f
Rahul Horé <hore.rahul@gmail.com>
2015-01-07 21:18:04 -0500

    Tries to configure to work with passenger

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,9 @@ Bundler.require(*Rails.groups)
 module SimpleChat
   class Application < Rails::Application
     config.middleware.delete Rack::Lock
-    config.middleware.use FayeRails::Middleware, mount: '/faye', timeout: 25
+    config.middleware.use FayeRails::Middleware,
+    mount: '/faye', timeout: 25, server: 'passenger',
+    engine: { type: Faye::Redis, host: 'localhost' }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
