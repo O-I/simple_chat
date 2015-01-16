974539bc120421a465378d17c59f22fe274c350b
Rahul Horé <hore.rahul@gmail.com>
2015-01-15 20:39:51 -0500

    Simplifies comments#create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,22 +11,9 @@ class CommentsController < ApplicationController
   end
 
   def create
-    respond_to do |format|
-      if current_user
-        @comment = current_user.comments.build(comment_params)
-
-        if @comment.save
-          flash.now[:success] = 'Your comment was successfully posted'
-        else
-          flash.now[:error] = 'Your comment could not be saved'
-        end
-        
-        format.html { redirect_to root_url }
-        format.js
-      else
-        format.html { redirect_to root_url }
-        format.js { render nothing: true }
-      end
+    if current_user
+      @comment = current_user.comments.build(comment_params)
+      @comment.save
     end
   end
 
