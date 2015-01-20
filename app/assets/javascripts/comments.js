35608a1e5032999b8c0cba6b3a30a528e5009a82
Rahul Horé <hore.rahul@gmail.com>
2015-01-19 22:11:41 -0500

    Passes data to client as HTML instead of as JSON

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -4,18 +4,7 @@ var source;
 source = new EventSource('/comments');
 
 source.onmessage = function(event) {
-  var comment, comment_template;
-  comment_template = _.template($('#comment_temp').html());
-  comment = $.parseJSON(event.data);
-  if (comment) {
-    return $('#comments').find('.media-list').prepend(comment_template({
-      body: comment['body'],
-      user_name: comment['user_name'],
-      user_avatar: comment['user_avatar'],
-      user_profile: comment['user_profile'],
-      timestamp: comment['timestamp']
-    }));
-  }
+  return $('#comments').find('.media-list').prepend($.parseHTML(event.data));
 };
 
 jQuery(function() {
